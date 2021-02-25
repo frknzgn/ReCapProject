279632c3cb766eca4d7defbdf6df19da1ffa6e35
selofuko@outlook.com <selofuko@outlook.com>
2021-02-25 16:25:44 +0300

    25.02.2021 ödev

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 
 namespace Business.Concrete
 {
@@ -20,17 +26,13 @@ namespace Business.Concrete
             _carDal= carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.CarName.Length>=5 && car.DailyPrice>=0 )
-            {
-                _carDal.Add(car);
+
+            _carDal.Add(car);
                 return new SuccessResult(Messages.CarAdded);
-            }
-            else
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
+            
         }
 
         public IResult Delete(Car car)
@@ -41,10 +43,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            //if (DateTime.Now.Hour>=00 || DateTime.Now.Hour<=02)
-            //{
-            //    return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
-            //}
+            if (DateTime.Now.Hour>=00 || DateTime.Now.Hour<=02)
+            {
+                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
+            }
 
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarListed);
         }
